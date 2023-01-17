a5f3fdb249a1c705c6c977a9308e2c59cb9b729b
mehdi-douib <mehdi.douib@laplateforme.io>
2023-01-17 17:58:23 +0100

    commit

--- jour2/job5/script.js
+++ jour2/job5/script.js
@@ -6,4 +6,15 @@ window.addEventListener('scroll', function(event)
       let scrolled = (windowscroll / height) *100;
 
       footer.style.width = scrolled + "%";
-}) 
+
+
+if (scrolled >= 0 && scrolled <= 25) {
+      footer.style.backgroundColor = "rgb(0, 247, 255)";
+    } else if (scrolled > 25 && scrolled <= 50) {
+      footer.style.backgroundColor = "rgb(226, 139, 0)";
+    } else if (scrolled > 50 && scrolled <= 75) {
+      footer.style.backgroundColor = "rgb(226, 208, 0)";
+    } else if (scrolled > 75 && scrolled <= 100) {
+      footer.style.backgroundColor = "rgb(120, 208, 0)";
+    }
+  }) 
