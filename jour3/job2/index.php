b85ec503cfea809f74ee0e89eecfd61748700d6a
mehdi-douib <mehdi.douib@laplateforme.io>
2023-01-21 17:28:21 +0100

    commit

---
+++ jour3/job2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+    <header></header>
+    <main>
+        <div id="shuffled">
+            <!-- <img class="img" id="01" src="">
+            <img class="img" id="02" src="">
+            <img class="img" id="03" src="">
+            <img class="img" id="04" src="">
+            <img class="img" id="05" src="">
+            <img class="img" id="06" src=""> -->
+        </div>
+        <button id="shuffle">shuffe it</button>
+        <div id="ordered"></div>
+    </main>
+    <footer></footer>
+    
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src='script.js'></script>
+</body>
+</html>
---
+++ jour3/job2/script.js
@@ -0,0 +1,61 @@
+$(document).ready(() => {
+    $('#shuffle').click(
+        function(){
+            shuffling()
+            $('#results').remove();
+        }
+    );
+
+})
+
+function shuffling() {
+    var shuffleTab = getShuffleTab()
+
+    $("#shuffled").empty()
+    $("#ordered").empty()
+
+    shuffleTab.forEach(i =>{
+        $(`<img id='${i}' src='${i}.png'>`).appendTo("#shuffled"); $(`#${i}`).click(
+            function(event){
+                toggleDiv(this)
+                checkVictory()
+            })
+    })
+}
+
+function toggleDiv(element){ 
+    if (element.parentNode.id == 'shuffled'){
+        $(element).appendTo('#ordered');
+    } else {
+        $(element).appendTo('#ordered');
+    }
+}
+
+function getShuffleTab() {
+    var randomTable = []
+    while (randomTable.length < 6) {
+        var rdNumber = Math.trunc(Math.random() * 7)
+        if (!randomTable.includes(rdNumber) && rdNumber > 0){
+            randomTable.push(rdNumber)
+        }
+    }
+    return randomTable;
+}
+
+function checkVictory() {
+    var imgNodeList = document.querySelector('#ordered').childNodes;
+    // $('#results').remove();
+    if (imgNodeList.length == 6) {
+        var counter = 1;
+        imgNodeList.forEach(element => {
+            if (element.id.replace('arc', '') == counter) {
+                counter++
+            }
+        });
+        if (counter == 7) {
+            $('<h1 id="results">Vous gagnant houe/h1>').appendTo("body");
+        } else {
+            $('<h1 id="results">Devinez qui vient de perdret?</h1>').appendTo("body");
+        }
+    }
+}
