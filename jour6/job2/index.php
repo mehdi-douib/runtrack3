3315a3f3457e88d3a25c7fb798d7c58280339536
mehdi-douib <mehdi.douib@laplateforme.io>
2023-01-24 16:32:12 +0100

    commit

---
+++ jour6/job2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Jour06 - Job02</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes"/>
+    <link rel="stylesheet" media="" href="style.css" />
+
+</head>
+<body>
+    <h1> Hello </h1>
+<main>
+</main>
+</body>
---
+++ jour6/job3/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jour6 - Job3</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=yes" />
+    <link rel="stylesheet" media="" href="style.css" />
+</head>
+<body>
+    <section class="section">
+        <p>Hello</p>
+    </section>
+    <section class="section">
+        <p>Hello</p>
+    </section>
+    <section class="section">
+        <p>Hello</p>
+    </section>
+    <section class="section">
+        <p>Hello</p>
+    </section>
+    </div>
+</body>
